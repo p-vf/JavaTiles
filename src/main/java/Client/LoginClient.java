e07887356f7d34a3c3fd1b6124aabd1f46371b4d
Boran <boran.goekcen@stud.unibas.ch>
2024-03-17 18:14:57 +0100

    some fixes

--- src/main/java/Client/LoginClient.java
+++ src/main/java/Client/LoginClient.java
@@ -1,2 +1,32 @@
-package Client;public class LoginClient {
+package Client;
+import java.util.Scanner;
+public class LoginClient {
+    private final Scanner scanner = new Scanner(System.in);
+
+
+    public void closeScanner(){
+        scanner.close();
+    }
+
+    public String enterUsername() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter username:");
+        return scanner.nextLine();
+    }
+    public int enterLobbyNumber(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter lobby number:");
+        return scanner.nextInt();
+
+    }
+
+    public static void main(String[] args){
+        LoginClient login = new LoginClient();
+        System.out.println(login.enterUsername());
+        System.out.println(login.enterLobbyNumber());
+        login.closeScanner();
+    }
+
+
+
 }
--- src/main/java/Server/EchoServer.java
+++ src/main/java/Server/EchoServer.java
@@ -1,2 +1,26 @@
-package Server;public class EchoServer {
+package Server;
+
+import Server.EchoClientThread;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class EchoServer {
+    public static void main(String[] args) {
+        int cnt = 0;
+        try {
+            System.out.println("Warte auf Verbindung auf Port 8090..");
+            ServerSocket echod = new ServerSocket(8090);
+            while (true) {
+                Socket socket = echod.accept();
+                EchoClientThread eC = new EchoClientThread(++cnt, socket);
+                Thread eCT = new Thread(eC);
+                eCT.start();
+            }
+        } catch (IOException e) {
+            System.err.println(e.toString());
+            System.exit(1);
+        }
+    }
 }
