8103c6d3e3a71e16f9a2ee49888584526c3d07be
Pascal von Fellenberg <pascal.vonfellenberg@stud.unibas.ch>
2024-04-09 14:51:00 +0200

    fix(server, edgecases): Fix nullpointerexception when someone left server from lobby and another one in the lobby types /ready

--- src/main/java/server/Lobby.java
+++ src/main/java/server/Lobby.java
@@ -3,7 +3,6 @@ package server;
 import game.Tile;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import utils.NetworkUtils;
 
 import java.io.IOException;
 import java.util.ArrayList;
