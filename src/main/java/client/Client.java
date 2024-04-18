21b8ed7cd3f68b40a68812c35590ae28763deb71
robin <robin.goekcen@stud.unibas.ch>
2024-04-18 13:04:10 +0200

    feat: Further implementation of the GUI

--- src/main/java/client/Client.java
+++ src/main/java/client/Client.java
@@ -51,6 +51,7 @@ public class Client {
     private GUIThread guiThread; //Thread responsible for displaying the GUI
 
     private boolean lobby = false; //Whether the client is in a lobby or not
+    private static GUIGame guiGame; //The GUI of the game
 
 
     /**
@@ -64,6 +65,7 @@ public class Client {
         this.out = socket.getOutputStream();
         this.in = socket.getInputStream();
         guiThread = new GUIThread(this);
+        guiGame = new GUIGame(this);
     }
 
 
@@ -81,6 +83,7 @@ public class Client {
             Client client = new Client(sock);
             Thread gThread = new Thread(client.guiThread);
             gThread.start();
+            guiGame.start();
             InThread th = new InThread(client.in, client, client.guiThread);
             Thread iT = new Thread(th);
             iT.start();
--- src/main/java/client/GUIGame.java
+++ src/main/java/client/GUIGame.java
@@ -1,10 +1,16 @@
 package client;
 
+import server.Lobby;
+
 import javax.swing.*;
 import javax.swing.border.Border;
+import java.awt.*;
 import java.io.IOException;
+import java.util.ArrayList;
 
-public class GUIGame extends JFrame implements Runnable {
+import static utils.NetworkUtils.encodeProtocolMessage;
+
+public class GUIGame {
 
     private JFrame frame;
     private JScrollPane scroll;
@@ -15,10 +21,35 @@ public class GUIGame extends JFrame implements Runnable {
     public GUIGame(Client client) {
         this.client = client;
     }
-    @Override
-    public void run() {
-        frame = new JFrame("JavaTiles");
+
+    public void start() {
+        frame = new JFrame("GameJavaTiles");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(800, 600);
+        JPanel panel = new JPanel();
+        JButton button = new JButton("Click me");
+        JButton button2 = new JButton("create Lobby");
+        panel.add(button2);
+        frame.add(panel);
+
+        button2.addActionListener(e -> {
+            try {
+
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+        button.addActionListener(e ->{System.out.println("Button clicked");});
+        panel.add(button);
+        frame.add(panel);
+        border = BorderFactory.createLineBorder(Color.BLACK, 1, true);
+        frame.setVisible(true);
+        frame.pack();
     }
 
+    public void commandToServer(String input) throws IOException {
+        client.handleInput(input);
+    }
 
 }
+
