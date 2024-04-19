2841097a81e7689dd05ed25be5ec5717378fb6f8
Boran <boran.goekcen@stud.unibas.ch>
2024-04-19 10:46:45 +0200

    feat(GUI,GUIMain): add GUI to its own Thrad

--- src/main/java/client/GUIMain.java
+++ src/main/java/client/GUIMain.java
@@ -1,19 +1,10 @@
 package client;
 
-import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.scene.layout.StackPane;
-import javafx.stage.Stage;
+public class GUIMain {
 
-public class GUIMain extends Application {
-
-    @Override
-    public void start(Stage primaryStage) {
-        primaryStage.setTitle("Simple Window");
-        StackPane root = new StackPane();
-        Scene scene = new Scene(root, 300, 250);
-        primaryStage.setScene(scene);
-        primaryStage.show();
+    public static void main(String[] args) {
+        GUI gui = new GUI();
+        Thread myThread = new Thread(gui);
+        myThread.start();
     }
-
 }
