0936bbf6821fd68d876313a0acd092c3fa064e3c
robin <robin.goekcen@stud.unibas.ch>
2024-04-21 21:54:40 +0200

    feat(gui, Controller): Switch to lobbyscreen after login implemented

--- src/main/java/gui/Controller.java
+++ src/main/java/gui/Controller.java
@@ -2,10 +2,16 @@ package gui;
 
 import client.Client;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.net.URL;
 
 import static utils.NetworkUtils.encodeProtocolMessage;
 
@@ -17,6 +23,10 @@ public class Controller {
 
     public static Client client;
 
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+
     public Controller() {
        //Default constructor with no parameters
     }
@@ -26,12 +36,23 @@ public class Controller {
     }
 
 
-
-    public void handleButtonClick() throws IOException {
+    public void handleButtonClick(ActionEvent event) throws IOException {
         System.out.println(input);
         client.send("LOGI " + input);
+        switchToLobbyScene(event);
+    }
 
-
+    private void switchToLobbyScene(ActionEvent event) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/lobby.fxml"));
+            Parent root = loader.load();
+            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 
@@ -40,5 +61,12 @@ public class Controller {
         input = myTextField.getText();
 
 
+    }
+
+    public void createPressed(ActionEvent actionEvent) {
+
+    }
+
+    public void joinPressed(ActionEvent actionEvent) {
     }
 }
