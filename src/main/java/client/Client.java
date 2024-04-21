bbd8110da7e605cdd3d6be5d83cb81aecb1d9827
robin <robin.goekcen@stud.unibas.ch>
2024-04-21 17:22:58 +0200

    feat(gui, LoginController): Further implementation loginwindow

--- src/main/java/client/Client.java
+++ src/main/java/client/Client.java
@@ -6,6 +6,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 import game.Tile;
+import gui.ControllerLogin;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
--- src/main/java/gui/ControllerLogin.java
+++ src/main/java/gui/ControllerLogin.java
@@ -1,5 +1,29 @@
 package gui;
 
+import client.Client;
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
 public class ControllerLogin {
 
+
+    public Button buttonSend;
+    public TextField myTextField;
+    public static String input;
+    public Client client;
+
+
+    public void handleButtonClick() {
+        System.out.println(input);
+
+    }
+
+
+
+    public void handleTextfield(ActionEvent event) {
+        input = myTextField.getText();
+
+
+    }
 }
