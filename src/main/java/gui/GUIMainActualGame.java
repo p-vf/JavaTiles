6440e8f1c474279f794ae8e09d821302e349ac53
robin <robin.goekcen@stud.unibas.ch>
2024-04-28 18:16:56 +0200

    docs(gui, GameGUI): Javadoc added

--- src/main/java/gui/GUIMainActualGame.java
+++
@@ -1,9 +0,0 @@
-package gui;
-
-public class GUIMainActualGame {
-    public static void main(String[] args) {
-        GameGUIActualGame gameGUIActualGame = new GameGUIActualGame();
-        Thread myThread = new Thread(gameGUIActualGame);
-        myThread.start();
-    }
-}
